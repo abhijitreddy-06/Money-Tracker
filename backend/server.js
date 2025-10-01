ac501a47289e071eabeedd988a9345b819dd0ee8
abhijitreddy-06 <abhijitreddy828@gmail.com>
2025-10-01 13:46:20 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,367 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import pg from 'pg';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+//env config
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const JWT_SECRET = process.env.JWT_SECRET || 'abhi_secret';
+
+//middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//db connection
+const pool = new pg.Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+// Test the database connection
+pool.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('Database connection error', err.stack));
+
+//auth middleware
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        if (!token || token === 'null') {
+            return res.status(401).json({ message: 'Token not provided.' });
+        }
+
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    return res.status(403).json({ message: 'Token has expired. Please log in again.' });
+                }
+                return res.status(403).json({ message: 'Token is invalid.' });
+            }
+
+           
+            req.user = decoded; 
+            console.log("Decoded JWT payload:", decoded);
+
+            next();
+        });
+    } else {
+        res.status(401).json({ message: 'Authorization header is missing.' });
+    }
+};
+
+//bcrypt salt rounds
+const saltRounds = 10;
+//user registration
+app.post('/register', (req, res) => {
+    const { name, phone, password } = req.body;
+    const myPlaintextPassword = password;
+    bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+        if (err) {
+            return res.status(500).json({ message: 'Error hashing password' });
+
+        }
+        pool.query('INSERT INTO users (name, phone, password) VALUES ($1, $2, $3)', [name, phone, hash], (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: 'Database error' });
+            }
+            res.status(201).json({ message: 'User registered successfully' });
+        });
+    });
+});
+
+
+//user login
+app.post('/login', (req, res) => {
+    const { phone, password } = req.body;
+
+    if (!phone || !password) {
+        return res.status(400).json({ message: 'Phone and password are required' });
+    }
+
+    pool.query('SELECT * FROM users WHERE phone = $1', [phone], (err, result) => {
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).json({ message: 'Database error' });
+        }
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'User not found. Please create an account.' });
+        }
+
+        const user = result.rows[0];
+        console.log(user);
+        console.log("Decoded user:", req.user);
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) {
+                console.error('Password compare error:', err);
+                return res.status(500).json({ message: 'Error comparing passwords' });
+            }
+
+            if (!isMatch) {
+                return res.status(401).json({ message: 'Invalid password' });
+            }
+            console.log("Signing JWT with id:", user.id);
+            // Sign JWT
+            const token = jwt.sign({ userid: user.id }, JWT_SECRET, { expiresIn: '1h' });
+            console.log("Generated Token:", token);
+            // Return token and user info (optional)
+            res.json({
+                token,
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    phone: user.phone
+                },
+                message: 'Login successful'
+            });
+        });
+    });
+});
+
+
+//current balance
+app.post('/balance', authenticateToken, async (req, res) => {
+    const { balance } = req.body;
+    const userid = req.user.userid;
+
+    try {
+        await pool.query(`
+            INSERT INTO users_balance (userid, balance) 
+            VALUES ($1, $2)
+            ON CONFLICT (userid) 
+            DO UPDATE SET balance = EXCLUDED.balance
+        `, [userid, balance]);
+
+        res.json({ message: 'Balance updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+// Check if balance exists for user
+app.get('/check-balance', authenticateToken, async (req, res) => {
+    const userid = req.user.userid;
+
+    try {
+        const result = await pool.query(
+            'SELECT balance FROM users_balance WHERE userid = $1',
+            [userid]
+        );
+
+        if (result.rows.length === 0) {
+            return res.json({ hasBalance: false });
+        }
+
+        return res.json({ hasBalance: true, balance: result.rows[0].balance });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+
+//spent money
+app.post('/spend', authenticateToken, async (req, res) => {
+    const { amount, for_what, place, date } = req.body;
+    const userid = req.user.userid;
+
+    try {
+        await pool.query(
+            'INSERT INTO user_spend (user_id, amount, for_what, place, spend_date) VALUES ($1, $2, $3, $4, $5)',
+            [userid, amount, for_what, place, date]
+        );
+
+        await pool.query(
+            'UPDATE users_balance SET balance = balance - $1 WHERE userid = $2',
+            [amount, userid]
+        );
+
+        res.json({ message: 'Spend record added & balance updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+
+
+//lend money
+app.post('/lend', authenticateToken, async (req, res) => {
+    const { amount, to_whom, return_date } = req.body;
+    const user_id = req.user.userid; // JWT payload
+
+    try {
+        // Insert lend record
+        await pool.query(
+            'INSERT INTO user_lend (user_id, amount, to_whom, return_date) VALUES ($1, $2, $3, $4)',
+            [user_id, amount, to_whom, return_date]
+        );
+
+        // Deduct from balance
+        await pool.query(
+            'UPDATE users_balance SET balance = balance - $1 WHERE userid = $2',
+            [amount, user_id]
+        );
+
+        res.json({ message: 'Lend record added & balance updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+
+
+
+//borrow money
+app.post('/borrow', authenticateToken, async (req, res) => {
+    const { amount, for_what, from_whom, return_date } = req.body;
+    const userid = req.user.userid;
+
+    try {
+        // Insert borrow record
+        await pool.query(
+            'INSERT INTO user_borrow (userid, amount, for_what, from_whom, return_date) VALUES ($1, $2, $3, $4, $5)',
+            [userid, amount, for_what, from_whom, return_date]
+        );
+
+        // Add to balance
+        await pool.query(
+            'UPDATE users_balance SET balance = balance + $1 WHERE userid = $2',
+            [amount, userid]
+        );
+
+        // Send a single response
+        res.json({ message: 'Borrow recorded and balance updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+
+
+//deposit money
+app.post('/deposit', authenticateToken, async (req, res) => {
+    const { amount, fromWhom, date } = req.body;
+    const userid = req.user.userid;
+
+    try {
+        // Insert deposit record
+        await pool.query(
+            'INSERT INTO user_deposit (user_id, amount, source, deposit_date) VALUES ($1, $2, $3, $4)',
+            [userid, amount, fromWhom, date]
+        );
+
+        // Update user's balance
+        await pool.query(
+            'UPDATE users_balance SET balance = balance + $1 WHERE userid = $2',
+            [amount, userid]
+        );
+
+        res.json({ message: 'Deposit recorded and balance updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+
+
+//history
+
+app.get('/history', authenticateToken, async (req, res) => {
+    const user_id = req.user.userid;
+
+    const query = `
+    SELECT 
+        'Spent' AS type,
+        amount,
+        for_what AS description,
+        place AS details,
+        spend_date AS date
+    FROM user_spend
+    WHERE user_id = $1
+
+    UNION ALL
+
+    SELECT
+        'Lent' AS type,
+        amount,
+        to_whom AS description,
+        'Return by ' || return_date AS details,
+        COALESCE(created_at, NOW()) AS date
+    FROM user_lend
+    WHERE user_id = $1
+
+    UNION ALL
+
+    SELECT
+        'Borrowed' AS type,
+        amount,
+        from_whom AS description,
+        'Due by ' || return_date AS details,
+        COALESCE(created_at, NOW()) AS date
+    FROM user_borrow
+    WHERE userid = $1
+
+    UNION ALL
+
+    SELECT
+        'Deposit' AS type,
+        amount,
+        source AS description,
+        NULL AS details,
+        deposit_date AS date
+    FROM user_deposit
+    WHERE user_id = $1
+
+    ORDER BY date DESC;
+    `;
+
+    try {
+        const { rows } = await pool.query(query, [user_id]);
+        res.json(rows);
+    } catch (err) {
+        console.error('History fetch error:', err);
+        res.status(500).json({ message: 'Database error' });
+    }
+});
+
+
+
+
+
+
+app.get('/lend', authenticateToken, (req, res) => {
+    res.json({ message: 'This is a protected route', user: req.user });
+});
+app.get('/home', authenticateToken, (req, res) => {
+    res.json({ message: 'This is a protected route', user: req.user });
+});
+
+app.get('/borrow', authenticateToken, (req, res) => {
+    res.json({ message: 'This is a protected route', user: req.user });
+});
+app.get('/profile', authenticateToken, (req, res) => {
+    res.json({ message: 'This is a protected route', user: req.user });
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'login.html'));
+});
+
+app.listen(3000, '0.0.0.0', () => console.log('Server running on port 3000'));
+
